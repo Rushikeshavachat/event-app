5444285e3e893ffb5d084a8c5308e773164428e2
Rushikeshavachat <119668833+Rushikeshavachat@users.noreply.github.com>
2024-04-28 10:33:20 +0530

    [first commit]

--- types/index.ts
+++ types/index.ts
@@ -1,11 +1,11 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
-  clerkId: string
-  firstName: string
-  lastName: string
-  username: string
-  email: string
-  photo: string
+  clerkId: string;
+   email: string; 
+   username: string;
+    firstName: string | null;
+     lastName: string | null;
+     photo: string;
 }
 
 export type UpdateUserParams = {
